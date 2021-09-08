a8b7b736cb05307c7d1d6b06e36e1b0704ce9688
Dan Gohman <dev@sunfishcode.online>
2021-09-08 15:19:29 -0700

    Add a spec reference for `__environ`.

--- c-scape/src/environ.rs
+++ c-scape/src/environ.rs
@@ -42,6 +42,8 @@ static INIT_ARRAY: unsafe extern "C" fn(c_int, *mut *mut c_char, *mut *mut c_cha
 /// original environment from the kernel, so we can find the auxv array in
 /// memory after it. Use priority 99 so that we run before any normal
 /// user-defined constructor functions.
+///
+/// <https://refspecs.linuxbase.org/LSB_5.0.0/LSB-Core-generic/LSB-Core-generic/baselib---environ.html>
 #[cfg(not(target_env = "gnu"))]
 #[used]
 #[link_section = ".init_array.00099"]
@@ -66,7 +68,6 @@ fn init_from_envp(envp: *mut *mut c_char) {
     unsafe { environ = envp };
 }
 
-#[cfg(target_env = "gnu")]
 #[no_mangle]
 #[used]
 pub static mut environ: *mut *mut c_char = null_mut();
