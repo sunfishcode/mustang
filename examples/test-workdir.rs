23b4c68838840b44f94513c3dd5589fe74c65ec9
Cole Miller <git@cole-miller.net>
2021-09-18 12:48:46 -0400

    Add test for getcwd and chdir

---
+++ examples/test-workdir.rs
@@ -0,0 +1,12 @@
+mustang::can_compile_this!();
+
+fn main() {
+    let _cwd = std::env::current_dir().expect("current directory should exist and be accessible");
+    let home = std::env::var_os("HOME").expect("HOME should be present in the environment");
+    std::env::set_current_dir(&home).expect("should be able to change to home directory");
+    let cwd = std::env::current_dir().expect("home directory should exist and be accessible");
+    assert_eq!(
+        cwd, home,
+        "should be in home directory after set_current_dir"
+    );
+}
