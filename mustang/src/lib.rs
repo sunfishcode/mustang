3844c51da59185702ca2d07e294d84bf63bc2669
Dan Gohman <dev@sunfishcode.online>
2021-10-04 22:28:10 -0700

    Disable unwind on x86 in another place.

--- mustang/src/lib.rs
+++ mustang/src/lib.rs
@@ -14,6 +14,7 @@ macro_rules! can_run_this {
 extern crate c;
 #[cfg(target_vendor = "mustang")]
 extern crate origin;
+#[cfg(not(target_arch = "x86"))]
 #[cfg(target_vendor = "mustang")]
 extern crate unwind;
 
