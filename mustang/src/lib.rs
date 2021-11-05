f04699f839935633752a758f9c96dc2e504d7251
Niv Kaminer <nivkner@zoho.com>
2021-11-05 14:45:48 +0200

    move unwinding to c-scape

--- mustang/src/lib.rs
+++ mustang/src/lib.rs
@@ -14,9 +14,6 @@ macro_rules! can_run_this {
 extern crate c_scape;
 #[cfg(target_vendor = "mustang")]
 extern crate origin;
-#[cfg(not(any(target_arch = "aarch64", target_arch = "arm", target_arch = "riscv64")))]
-#[cfg(target_vendor = "mustang")]
-extern crate unwinding;
 
 // global allocator must be defined in the mustang crate,
 // to ensure symbols from the above crates are actually linked
