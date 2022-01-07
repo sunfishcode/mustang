a455f8dacd49fd168daf9683c854c0da61b3f09c
Ben Struhl <benstruhl@Bens-MacBook-Pro.local>
2022-01-06 23:02:10 -0500

    feature request

--- c-scape/src/lib.rs
+++ c-scape/src/lib.rs
@@ -1614,6 +1614,21 @@ unsafe extern "C" fn pipe2(pipefd: *mut c_int, flags: c_int) -> c_int {
     }
 }
 
+#[no_mangle]
+unsafe extern "C" fn pipe(pipefd: *mut c_int) -> c_int {
+    libc!(libc::pipe(pipefd));
+
+    let flags = PipeFlags::from_bits(flags as _).unwrap();
+    match convert_res(rustix::io::pipe_with(flags)) {
+        Some((a, b)) => {
+            *pipefd = a.into_raw_fd();
+            *pipefd.add(1) = b.into_raw_fd();
+            0
+        }
+        None => -1,
+    }
+}
+
 #[cfg(any(target_os = "android", target_os = "linux"))]
 #[no_mangle]
 unsafe extern "C" fn sendfile() {
