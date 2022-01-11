ef17639de3e518befdd93f9f8d608927ad5de4c1
Niv Kaminer <nivkner@zoho.com>
2022-01-11 13:14:11 +0200

    style changes to comments

--- c-scape/src/use_libc.rs
+++ c-scape/src/use_libc.rs
@@ -1,14 +1,14 @@
 //! Utilities to check against C signatures, when enabled.
 
-/// casts the given pointer to another type, similarly to calling `cast()` on it,
-/// while verifying that the layout of the pointee stays the same after the cast
+/// Casts the given pointer to another type, similarly to calling `cast()` on it,
+/// while verifying that the layout of the pointee stays the same after the cast.
 macro_rules! checked_cast {
     ($ptr:ident) => {{
         let target_ptr = $ptr.cast();
         let target = Pad::new(core::ptr::read(target_ptr));
 
-        // uses the fact that the compiler checks for size equality,
-        // when transmuting between types
+        // Uses the fact that the compiler checks for size equality,
+        // when transmuting between types.
         let size_check = core::mem::transmute(core::ptr::read($ptr));
         target.compare_size(size_check);
         let align_check = core::mem::transmute(Pad::new(core::ptr::read($ptr)));
@@ -48,14 +48,14 @@ macro_rules! libc_type {
     };
 }
 
-// a struct that adds align_of<T> padding bytes to type T
-//
-// based of the rules of C struct alignment:
-// align_of<Pad<T>> == max(align_of<T>, align_of<u8>) == align_of<T>,
-// size_of<Pad<T>> / align_of<Pad<T>>
-// == ciel( (size_of<T> + size_of<u8>) / align_of<Pad<T>>)
-// and size_of<T> is divisible by align_of<T>
-// therefore size_of<Pad<T>> == size_of<T> + align_of<T>
+/// A struct that adds `align_of<T>` padding bytes to type `T`.
+///
+/// Based on the rules of C struct alignment:
+/// * `align_of<Pad<T>> == max(align_of<T>, align_of<u8>) == align_of<T>`
+/// * `size_of<Pad<T>> / align_of<Pad<T>> == ciel( (size_of<T> + size_of<u8>) / align_of<Pad<T>>)`
+/// * `size_of<T> % align_of<T> == 0`
+///
+/// Therefore `size_of<Pad<T>> == size_of<T> + align_of<T>`.
 #[repr(C)]
 pub(crate) struct Pad<T> {
     field: T,
@@ -69,10 +69,11 @@ impl<T> Pad<T> {
             force_padding: 0,
         }
     }
-    // used to check that size_of<T> == size_of<U> using transmute,
+    /// Used to check that `size_of<T> == size_of<U>` with `transmute`.
     pub fn compare_size(&self, _v: T) {}
-    // used to check that size_of<Pad<T>> == size_of<Pad<U>> using transmute,
-    // and since size_of<Pad<T>> == size_of<T> + align_of<T>,
-    // if size_of<T> == size_of<U> then align_of<T> == align_of<U>
+    /// Used to check that `size_of<Pad<T>> == size_of<Pad<U>>` with `transmute`.
+    ///
+    /// Since `size_of<Pad<T>> == size_of<T> + align_of<T>`,
+    /// if `size_of<T> == size_of<U>` then `align_of<T> == align_of<U>`.
     pub fn compare_alignment(&self, _pad: Pad<T>) {}
 }
