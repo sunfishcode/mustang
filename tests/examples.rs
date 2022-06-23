a01de7a130754a81beeb35a031c63babfe8db3c4
Dan Gohman <dev@sunfishcode.online>
2022-06-23 14:21:25 -0700

    Fix warnings.
    Fix an unused-variable warning in origin/src/threads.rs in release builds.
    
    And remove the has-elf-tls keys from the target descriptions, which
    are no longer needed. And re-enable stderr checking in the examples
    test now that we no longer get warnings.

--- tests/examples.rs
+++ tests/examples.rs
@@ -58,9 +58,6 @@ fn test_example(name: &str, features: &str, stdout: &str, stderr: &str) {
         .arg(name);
     let output = command.output().unwrap();
 
-    // FIXME: Re-enable this once we no longer get "warning: target json file
-    // contains unused fields: has-thread-local" warnings.
-    /*
     assert_eq_str!(
         stderr.as_bytes(),
         &output.stderr,
@@ -68,7 +65,6 @@ fn test_example(name: &str, features: &str, stdout: &str, stderr: &str) {
         name,
         output
     );
-    */
 
     assert_eq_str!(
         stdout.as_bytes(),
