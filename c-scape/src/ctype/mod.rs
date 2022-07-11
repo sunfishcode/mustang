a6b496bddd86cfb1af2554534f2817371ccb458b
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2022-07-11 13:11:50 -0500

    Review nits for ctype functions

--- c-scape/src/ctype/mod.rs
+++ c-scape/src/ctype/mod.rs
@@ -1,66 +1,81 @@
 #[cfg(all(target_os = "linux", target_env = "gnu"))]
 mod lsb_abi;
 
-use core::ops::RangeInclusive;
 use libc::c_int;
 
-#[no_mangle]
-extern "C" fn isalnum(c: c_int) -> c_int {
-    (isdigit(c) != 0 || isalpha(c) != 0) as c_int
+// This is correct because these functions
+// expect the value to be representable
+// in an unsigned char, or be EOF, or else
+// UB occurs.
+//
+// EOF is -1, which when truncated to u8
+// will be 255, correctly returning false for
+// all of these functions.
+fn truncate_to_u8(c: c_int) -> u8 {
+    c as u8
 }
 
 #[no_mangle]
-extern "C" fn isalpha(c: c_int) -> c_int {
-    (isupper(c) != 0 || islower(c) != 0) as c_int
+unsafe extern "C" fn isalnum(c: c_int) -> c_int {
+    libc!(libc::isalnum(c));
+    truncate_to_u8(c).is_ascii_alphanumeric() as c_int
 }
 
 #[no_mangle]
-extern "C" fn isascii(c: c_int) -> c_int {
-    const RANGE: RangeInclusive<c_int> = 0..=127;
-    RANGE.contains(&c) as c_int
+unsafe extern "C" fn isalpha(c: c_int) -> c_int {
+    libc!(libc::isalpha(c));
+    truncate_to_u8(c).is_ascii_alphabetic() as c_int
 }
 
 #[no_mangle]
-extern "C" fn isblank(c: c_int) -> c_int {
+unsafe extern "C" fn isblank(c: c_int) -> c_int {
+    libc!(libc::isblank(c));
     (c == ' ' as c_int || c == '\t' as c_int) as c_int
 }
 
 #[no_mangle]
-extern "C" fn iscntrl(c: c_int) -> c_int {
-    const LOWER_RANGE: RangeInclusive<c_int> = 0..=31;
-    (LOWER_RANGE.contains(&c) || c == 127) as c_int
+unsafe extern "C" fn iscntrl(c: c_int) -> c_int {
+    libc!(libc::iscntrl(c));
+    truncate_to_u8(c).is_ascii_control() as c_int
 }
 
 #[no_mangle]
-extern "C" fn isdigit(c: c_int) -> c_int {
-    const RANGE: RangeInclusive<c_int> = ('0' as c_int)..=('9' as c_int);
-    RANGE.contains(&c) as c_int
+unsafe extern "C" fn isdigit(c: c_int) -> c_int {
+    libc!(libc::isdigit(c));
+    truncate_to_u8(c).is_ascii_digit() as c_int
 }
 
 #[no_mangle]
-extern "C" fn isgraph(c: c_int) -> c_int {
-    const RANGE: RangeInclusive<c_int> = 33..=126;
-    RANGE.contains(&c) as c_int
+unsafe extern "C" fn isgraph(c: c_int) -> c_int {
+    libc!(libc::isgraph(c));
+    truncate_to_u8(c).is_ascii_graphic() as c_int
 }
 
 #[no_mangle]
-extern "C" fn islower(c: c_int) -> c_int {
-    const RANGE: RangeInclusive<c_int> = ('a' as c_int)..=('z' as c_int);
-    RANGE.contains(&c) as c_int
+unsafe extern "C" fn islower(c: c_int) -> c_int {
+    libc!(libc::islower(c));
+    truncate_to_u8(c).is_ascii_lowercase() as c_int
 }
 
 #[no_mangle]
-extern "C" fn isprint(c: c_int) -> c_int {
+unsafe extern "C" fn isprint(c: c_int) -> c_int {
+    libc!(libc::isprint(c));
     (c == 32 || isgraph(c) != 0) as c_int
 }
 
 #[no_mangle]
-extern "C" fn ispunct(c: c_int) -> c_int {
-    (isgraph(c) != 0 && isalnum(c) == 0) as c_int
+unsafe extern "C" fn ispunct(c: c_int) -> c_int {
+    libc!(libc::ispunct(c));
+    truncate_to_u8(c).is_ascii_punctuation() as c_int
 }
 
 #[no_mangle]
-extern "C" fn isspace(c: c_int) -> c_int {
+unsafe extern "C" fn isspace(c: c_int) -> c_int {
+    libc!(libc::isspace(c));
+
+    // We can't use the core function
+    // for space testing here because we 
+    // have a slightly different defintion.
     let is_space = c == ' ' as c_int
         || c == '\t' as c_int
         || c == '\n' as c_int
@@ -72,34 +87,25 @@ extern "C" fn isspace(c: c_int) -> c_int {
 }
 
 #[no_mangle]
-extern "C" fn isupper(c: c_int) -> c_int {
-    const RANGE: RangeInclusive<c_int> = ('A' as c_int)..=('Z' as c_int);
-    RANGE.contains(&c) as c_int
-}
-
-const ALPHA_CASE_BIT: c_int = 0x20;
-
-#[no_mangle]
-extern "C" fn isxdigit(c: c_int) -> c_int {
-    const HEX_LOWER_RANGE: RangeInclusive<c_int> = ('a' as c_int)..=('f' as c_int);
-    let lowered_unchecked = c | ALPHA_CASE_BIT;
-    (isdigit(c) != 0 || HEX_LOWER_RANGE.contains(&lowered_unchecked)) as c_int
+unsafe extern "C" fn isupper(c: c_int) -> c_int {
+    libc!(libc::isupper(c));
+    truncate_to_u8(c).is_ascii_uppercase() as c_int
 }
 
 #[no_mangle]
-extern "C" fn tolower(c: c_int) -> c_int {
-    if isupper(c) != 0 {
-        c | ALPHA_CASE_BIT
-    } else {
-        c
-    }
+unsafe extern "C" fn isxdigit(c: c_int) -> c_int {
+    libc!(libc::isxdigit(c));
+    truncate_to_u8(c).is_ascii_hexdigit() as c_int
 }
 
 #[no_mangle]
-extern "C" fn toupper(c: c_int) -> c_int {
-    if islower(c) != 0 {
-        c & !ALPHA_CASE_BIT
-    } else {
-        c
-    }
+unsafe extern "C" fn tolower(c: c_int) -> c_int {
+    libc!(libc::tolower(c));
+    truncate_to_u8(c).to_ascii_lowercase() as c_int
+}
+
+#[no_mangle]
+unsafe extern "C" fn toupper(c: c_int) -> c_int {
+    libc!(libc::toupper(c));
+    truncate_to_u8(c).to_ascii_uppercase() as c_int
 }
