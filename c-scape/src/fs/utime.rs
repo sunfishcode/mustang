01d9d9eaf13a5e16d7b9f116448bfb81cad13684
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2022-07-13 21:17:12 -0500

    Add utimes

--- c-scape/src/fs/utime.rs
+++ c-scape/src/fs/utime.rs
@@ -1,10 +1,12 @@
 use core::ffi::CStr;
+use core::ptr;
 use rustix::fd::BorrowedFd;
 use rustix::fs::AtFlags;
 
 use libc::{c_char, c_int};
 
 use crate::convert_res;
+use errno::{set_errno, Errno};
 
 #[no_mangle]
 unsafe extern "C" fn futimens(fd: c_int, times: *const libc::timespec) -> c_int {
@@ -44,4 +46,37 @@ unsafe extern "C" fn utimensat(
         Some(()) => 0,
         None => -1,
     }
+}
+
+#[no_mangle]
+unsafe extern "C" fn utimes(path: *const c_char, times: *const libc::timeval) -> c_int {
+    libc!(libc::utimes(path, times));
+
+    let mut arr: [libc::timespec; 2] = core::mem::zeroed();
+
+    let times = times as *const [libc::timeval; 2];
+    if !times.is_null() {
+        for i in 0..2 {
+            arr[i].tv_sec = (*times)[i].tv_sec;
+
+            match (*times)[i].tv_usec.checked_mul(1000) {
+                Some(t) => arr[i].tv_nsec = t,
+                None => {
+                    set_errno(Errno(libc::EINVAL));
+                    return -1;
+                }
+            }
+        }
+    }
+
+    utimensat(
+        libc::AT_FDCWD,
+        path,
+        if times.is_null() {
+            ptr::null()
+        } else {
+            arr.as_ptr()
+        },
+        0,
+    )
 }
