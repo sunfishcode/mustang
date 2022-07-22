6bee26b5089d54347ed9b5e3fb0a1f8cfa81a52f
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2022-07-22 14:01:50 -0500

    Implement epoch-system for keys

--- c-scape/src/threads/key.rs
+++ c-scape/src/threads/key.rs
@@ -1,30 +1,49 @@
 use alloc::boxed::Box;
 use core::cell::Cell;
-use core::ptr::null_mut;
+use core::ptr::{null, null_mut};
+use core::sync::atomic::{AtomicU16, Ordering};
 use errno::{set_errno, Errno};
 use libc::{c_int, c_void};
 
-use origin::sync::Mutex;
+use origin::sync::RwLock;
 
 const PTHREAD_KEYS_MAX: u32 = 128;
 const PTHREAD_DESTRUCTOR_ITERATIONS: u8 = 4;
 
 #[derive(Clone, Copy)]
 struct KeyData {
-    current_key: libc::pthread_key_t,
+    next_key: libc::pthread_key_t,
     destructors: [Option<unsafe extern "C" fn(_: *mut c_void)>; PTHREAD_KEYS_MAX as usize],
 }
 
-static KEY_DATA: Mutex<KeyData> = Mutex::new(KeyData {
-    current_key: 0,
+#[derive(Clone, Copy)]
+struct ValueWithEpoch {
+    epoch: u16,
+    data: *mut c_void,
+}
+
+impl ValueWithEpoch {
+    const fn new() -> Self {
+        ValueWithEpoch {
+            epoch: 0,
+            data: null_mut(),
+        }
+    }
+}
+
+static KEY_DATA: RwLock<KeyData> = RwLock::new(KeyData {
+    next_key: 0,
     destructors: [None; PTHREAD_KEYS_MAX as usize],
 });
 
-const VALUES_INIT: Cell<*mut c_void> = Cell::new(null_mut());
+static EPOCHS: [AtomicU16; PTHREAD_KEYS_MAX as usize] =
+    [const { AtomicU16::new(0) }; PTHREAD_KEYS_MAX as usize];
 
+// This uses an epoch-based system for differentiating between
+// reused keys corresponding to the same slot.
 #[thread_local]
-static VALUES: [Cell<*mut c_void>; PTHREAD_KEYS_MAX as usize] =
-    [VALUES_INIT; PTHREAD_KEYS_MAX as usize];
+static VALUES: [Cell<ValueWithEpoch>; PTHREAD_KEYS_MAX as usize] =
+    [const { Cell::new(ValueWithEpoch::new()) }; PTHREAD_KEYS_MAX as usize];
 
 #[thread_local]
 static HAS_REGISTERED_CLEANUP: Cell<bool> = Cell::new(false);
@@ -33,13 +52,25 @@ static HAS_REGISTERED_CLEANUP: Cell<bool> = Cell::new(false);
 unsafe extern "C" fn pthread_getspecific(key: libc::pthread_key_t) -> *mut c_void {
     libc!(libc::pthread_getspecific(key));
 
-    VALUES[key as usize].get()
+    let latest_epoch = EPOCHS[key as usize].load(Ordering::SeqCst);
+    let ValueWithEpoch { epoch, data } = VALUES[key as usize].get();
+
+    // If the latest epoch is newer, then that means this slot got reallocated.
+    // Either we are reusing a deleted key, which is UB, or we are reusing
+    // the new key, which must return null initially.
+    if epoch < latest_epoch {
+        null_mut()
+    } else {
+        data
+    }
 }
 
 #[no_mangle]
 unsafe extern "C" fn pthread_setspecific(key: libc::pthread_key_t, value: *const c_void) -> c_int {
     libc!(libc::pthread_setspecific(key, value));
 
+    // If this is the first-time we have gotten here,
+    // we need to actually register the dtors for cleanup.
     if !HAS_REGISTERED_CLEANUP.get() {
         // TODO: The ordering that we need here is
         // all the pthread_cleanup functions,
@@ -53,9 +84,9 @@ unsafe extern "C" fn pthread_setspecific(key: libc::pthread_key_t, value: *const
                 let mut ran_dtor = false;
 
                 for i in 0..PTHREAD_KEYS_MAX {
-                    let value_ptr = VALUES[i as usize].get();
+                    let data = pthread_getspecific(i as libc::pthread_key_t);
 
-                    if value_ptr.is_null() {
+                    if data.is_null() {
                         continue;
                     }
 
@@ -66,13 +97,19 @@ unsafe extern "C" fn pthread_setspecific(key: libc::pthread_key_t, value: *const
                         // be called within a destructor function...
                         // We have to take each dtor one at a time just
                         // in case someone did delete it;
-                        let key_data = KEY_DATA.lock();
+                        let key_data = KEY_DATA.read();
                         key_data.destructors[i as usize]
                     };
 
                     if let Some(dtor) = dtor {
-                        VALUES[i as usize].set(null_mut());
-                        dtor(value_ptr);
+                        // Null out the data as required
+                        // by POSIX semantics. This may bump
+                        // the local epoch, but that doesn't matter.
+                        pthread_setspecific(i as libc::pthread_key_t, null());
+
+                        // Call the destructor with the old
+                        // data, before we set it to null.
+                        dtor(value as *mut _);
                     }
                 }
 
@@ -85,7 +122,10 @@ unsafe extern "C" fn pthread_setspecific(key: libc::pthread_key_t, value: *const
         HAS_REGISTERED_CLEANUP.set(true);
     }
 
-    VALUES[key as usize].set(value as *mut _);
+    VALUES[key as usize].set(ValueWithEpoch {
+        epoch: EPOCHS[key as usize].load(Ordering::SeqCst),
+        data: value as *mut _,
+    });
     0
 }
 
@@ -96,16 +136,38 @@ unsafe extern "C" fn pthread_key_create(
 ) -> c_int {
     libc!(libc::pthread_key_create(key, dtor));
 
-    let mut key_data = KEY_DATA.lock();
+    extern "C" fn empty_dtor(_: *mut c_void) {}
 
-    let next_key = key_data.current_key;
-    if next_key >= PTHREAD_KEYS_MAX {
-        set_errno(Errno(libc::EAGAIN));
-        return -1;
+    let mut key_data = KEY_DATA.write();
+
+    let mut next_key = key_data.next_key;
+    if next_key < PTHREAD_KEYS_MAX {
+        // Fast-path, less than `PTHREAD_KEYS_MAX` slots
+        // have been allocated, we just use this as a bump
+        // allocator basically.
+        key_data.next_key = next_key + 1;
+    } else {
+        // Slow-path, linearly scan through the table to try
+        // and find an empty slot.
+        for (index, dtor) in key_data.destructors.iter().enumerate() {
+            if dtor.is_none() {
+                // We have to bump the epoch now that we are
+                // reusing slots.
+                EPOCHS[index].fetch_add(1, Ordering::SeqCst);
+                next_key = index as libc::pthread_key_t;
+            }
+        }
+
+        // If the loop still did not find a valid key
+        if next_key >= PTHREAD_KEYS_MAX {
+            set_errno(Errno(libc::EAGAIN));
+            return -1;
+        }
     }
 
-    key_data.current_key = next_key + 1;
-    key_data.destructors[next_key as usize] = dtor;
+    // We have to `unwrap_or` the dtor because `None` is reserved for signifying
+    // that the key is not allocated.
+    key_data.destructors[next_key as usize] = Some(dtor.unwrap_or(empty_dtor));
 
     0
 }
@@ -114,7 +176,7 @@ unsafe extern "C" fn pthread_key_create(
 unsafe extern "C" fn pthread_key_delete(key: libc::pthread_key_t) -> c_int {
     libc!(libc::pthread_key_delete(key));
 
-    let mut key_data = KEY_DATA.lock();
+    let mut key_data = KEY_DATA.write();
     key_data.destructors[key as usize] = None;
 
     0
