4ec00a70dc5a0935424e95caf4570b4f9dddb809
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2022-12-11 06:41:13 -0600

    Implement fchmodat

--- c-scape/src/fs/chmod.rs
+++ c-scape/src/fs/chmod.rs
@@ -2,27 +2,19 @@ use core::ffi::CStr;
 use rustix::fd::BorrowedFd;
 use rustix::fs::{cwd, Mode};
 
-use libc::{c_char, c_int, c_uint};
+use libc::{c_char, c_int, mode_t};
 
 use crate::convert_res;
 
 #[no_mangle]
-unsafe extern "C" fn chmod(pathname: *const c_char, mode: c_uint) -> c_int {
+unsafe extern "C" fn chmod(pathname: *const c_char, mode: mode_t) -> c_int {
     libc!(libc::chmod(pathname, mode));
 
-    let mode = Mode::from_bits((mode & !libc::S_IFMT) as _).unwrap();
-    match convert_res(rustix::fs::chmodat(
-        cwd(),
-        CStr::from_ptr(pathname.cast()),
-        mode,
-    )) {
-        Some(()) => 0,
-        None => -1,
-    }
+    fchmodat(libc::AT_FDCWD, pathname, mode, 0)
 }
 
 #[no_mangle]
-unsafe extern "C" fn fchmod(fd: c_int, mode: c_uint) -> c_int {
+unsafe extern "C" fn fchmod(fd: c_int, mode: mode_t) -> c_int {
     libc!(libc::fchmod(fd, mode));
 
     let mode = Mode::from_bits((mode & !libc::S_IFMT) as _).unwrap();
@@ -33,7 +25,25 @@ unsafe extern "C" fn fchmod(fd: c_int, mode: c_uint) -> c_int {
 }
 
 #[no_mangle]
-unsafe extern "C" fn fchmodat() {
-    // libc!(libc::fchmodat());
-    unimplemented!("fchmodat")
+unsafe extern "C" fn fchmodat(
+    fd: c_int,
+    pathname: *const c_char,
+    mode: mode_t,
+    flags: c_int,
+) -> c_int {
+    libc!(libc::fchmodat(fd, pathname, mode, flags));
+
+    if flags != 0 {
+        todo!("flags support in fchmodat");
+    }
+
+    let mode = Mode::from_bits((mode & !libc::S_IFMT) as _).unwrap();
+    match convert_res(rustix::fs::chmodat(
+        cwd(),
+        CStr::from_ptr(pathname.cast()),
+        mode,
+    )) {
+        Some(()) => 0,
+        None => -1,
+    }
 }
