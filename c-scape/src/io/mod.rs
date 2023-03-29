4a6abe0accbf76e86a09495ca25e793a612a4851
Dan Gohman <dev@sunfishcode.online>
2023-03-29 12:20:09 -0700

    Implement `epoll`
    Use the new epoll API from bytecodealliance/rustix#487.
    
    Fixes #86.

--- c-scape/src/io/mod.rs
+++ c-scape/src/io/mod.rs
@@ -1,7 +1,7 @@
 #[cfg(not(target_os = "wasi"))]
 mod dup;
-// #[cfg(any(target_os = "android", target_os = "linux"))]
-// mod epoll;
+#[cfg(any(target_os = "android", target_os = "linux"))]
+mod epoll;
 mod isatty;
 mod pipe;
 mod poll;
